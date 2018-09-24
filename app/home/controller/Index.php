c2b54fa78b5fc7586491c3dda9273372673a5c1d
wangjin <972270516@qq.com>
2018-09-24 20:17:37 +0800

    111

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -10,8 +10,83 @@ use think\Controller;
 
 class Index extends Controller
 {
-   public function index(){
-
+   public function index($id=0){
+     
+       
+       //查询部门列表
+       $bmlist=db('auth_group')->where(['type'=>2])->select();
+       
+       
+       //查询部门信箱总数
+       $order = "listorder desc,id desc";
+       if($id==0&&empty(input('keyword'))){
+           $id=$bmlist[0]['group_id'];
+       }
+       
+        
+        
+        $pageSize =input('limit')?input('limit'):10;
+        $order = "listorder desc,id desc"; 
+        if(input('post.pagelist')){
+            $num=input('post.pagelist');//post和get需要和前台提交的时候保持一致否则无效
+        }
+        if(input('keyword')){
+           $map=[
+           'deletetime'=>0,
+           'id'=>input('keyword')
+          ]; 
+        }else{
+            $map=[
+           'deletetime'=>0,
+           'department'=> $id
+          ];
+        }
+         
+        $count= Db('emailbox')->where($map)->count();//获取数据的总数量
+        $emaillist = db('emailbox')
+                ->where($map)
+                ->order($order)
+                ->paginate($pageSize,$count/*false*/,[//数字10为每页显示的总条数，true为去掉中间的页码部分，false为显示分页的页码
+            'type'       => 'page\Page',//分页类名
+            'var_page' => 'page',//分页变量
+            'page'     => $num,//传入跳转值给当前页
+           ]);
+             
+        //print_r($emaillist);exit;
+        $page=$emaillist->render();
+        
+        $listarr=$emaillist->toArray();
+        $emaillists=$listarr['data'];
+        foreach ($emaillists as $k=>$v ){
+               
+                $listarr['data'][$k]['createtime'] = date('Y-m-d',$v['createtime']);
+                if($v['is_reply']==1){
+                  $listarr['data'][$k]['replyname']='已回复';
+                }else{
+                  $listarr['data'][$k]['replyname']='未回复'; 
+                }
+            }
+        //print_r($emaillist);exit;
+        
+         $this->assign('page',$page);
+         if($id==0&&!empty(input('keyword'))){
+             
+           $this->assign('selectid',$listarr['data'][0]['department']);
+           $grouptitle=db('auth_group')->where(['group_id'=>$listarr['data'][0]['department']])->value('title');
+           $this->assign('selectname',$grouptitle);
+           }else{
+               $this->assign('selectid',$id);
+               $this->assign('selectname',$bmlist[0]['title']);
+               
+         }
+         $this->assign('emaillist',$listarr);
+         
+         $this->assign('bmlist',$bmlist);  
+      
+       
       return $this -> fetch();
+       
    }
+   
+  
 }
--- app/home/controller/Stdetails.php
+++ app/home/controller/Stdetails.php
@@ -8,11 +8,66 @@
 
 namespace app\home\controller;
 use think\Controller;
-
+use app\common\logic\ReplymessLogic;
 class Stdetails extends Controller
 {
-    public function index(){
-
-        return $this -> fetch();
+    
+    public function _initialize() {
+        //查询部门列表
+       $bmlist=db('auth_group')->where(['type'=>2])->select();
+        $this->assign('bmlist',$bmlist);  
     }
+    public function index(){
+        
+        
+        //接收id后是否需要密码
+        $id= request()->param('id');
+        $pwd= request()->param('pwd');
+        if($id){
+           $data= ReplymessLogic::getInstance()->getMessContent($id); //查询部门信件内容
+           if(!empty($data['mess'])){
+               $mess=$data['mess'];
+               if($mess['is_public']!=1){
+                  if(md5($pwd)!=$mess['pwd']) {
+                      
+                      $this->error('密码错误，将返回列表页', url('home/Index/index')) ;
+                  }
+               }
+               
+            //print_r($data);exit;
+            $this->assign('content',$data);
+             return $this -> fetch();
+           
+        
+          
+           }
+        }
+    }
+    
+    
+    //验证密码
+    public function validpwd() {
+        
+        $data=request()->param();
+        $data['pwd']= md5($data['pwd']);
+        $data['deletetime']=0;
+        
+        
+        $count=db('emailbox')->where($data)->count();
+        
+        if($count>0){
+            echo json_encode([
+                'code'=>1,
+                'msg'=>'密码正确',
+            ]);
+        }else{
+            echo json_encode([
+                'code'=>0,
+                'msg'=>'密码错误',
+            ]);
+        }
+        exit;
+    }
+    
+    
 }
